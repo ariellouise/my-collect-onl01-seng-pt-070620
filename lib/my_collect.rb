9d98133cc361d47d26dcb1d57b3fb69fb4ba8ab6
ariel murphy <arielladders@gmail.com>
2020-07-16 22:50:58 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -9,3 +9,18 @@ def my_collect(array)
   end
   languages
 end 
+
+
+
+
+
+
+ it "can handle an empty collection" do
+    empty_array = []
+    counter = 0
+      my_collect(empty_array) do |x|
+        counter += 1
+      end
+    expect(counter).to eq(0)
+  end
+
